1f920a230471b73a9e8ea2b4c504e87dc305dee3
Mahmoud Osama <osamamahmod657@gmail.com>
2025-01-07 22:59:36 +0000

    final project

--- models/Core/Fact_Transactions.sql
+++ models/Core/Fact_Transactions.sql
@@ -20,16 +20,16 @@ all_transacts as
     select trans.transaction_id,quantity,customer_id,store_id,promotion_id,transaction_time,total_amount,transaction_date,product_id
     from trans inner join prod_trans on trans.transaction_id=prod_trans.transaction_id
 )
-select concat(datekey,transaction_id,customer_id,promotion_id,category_id) as transaction_surr,
+select concat(datekey,transaction_id,customer_id,category_id) as transaction_surr,
 datekey,
 transaction_id,
 customer_id,
 store_id,
 promotion_id,
 category_id,
-quantity,
 transaction_time,
-total_amount,
-current_date() as current_time
+current_timestamp() as current_time,
+quantity,
+total_amount
 from all_transacts as a inner join datee as d on a.transaction_date = d.full_date
 inner join prods as p on p.product_id=a.product_id
